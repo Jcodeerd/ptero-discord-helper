c73f2d4a93b5a060ce360b7b14f1f2ea41266001
Jcodeerd <68161666+Jcodeerd@users.noreply.github.com>
2023-05-01 23:12:42 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/ash
+
+echo "Starting PHP-FPM..."
+/usr/sbin/php-fpm7 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
+
+echo "Starting Nginx..."
+/usr/sbin/nginx -c /home/container/nginx/nginx.conf
+
+echo "Starting Media-helper..."
+/usr/local/bin/node .
